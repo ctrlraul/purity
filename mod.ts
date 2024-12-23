a363dfe65b233880759a4a160524e81a01101f59
Raul <47065615+ctrlraul@users.noreply.github.com>
2024-12-23 06:57:59 -0300

    Added the ugly jsdoc for most stuff

--- mod.ts
+++ mod.ts
@@ -11,6 +11,25 @@ type CheckerType<C extends Checker> = C extends Checker<infer T> ? T : unknown;
 
 
 
+/** Type checker class used to create type checkers
+ * @example
+ * ```ts
+ * import { Checker } from '@purity/purity';
+ * 
+ * const $uint = new Checker<number>({
+ * 	typeString: 'uint',
+ * 	check(value) {
+ * 		return typeof value === 'number' && value >= 0;
+ * 	},
+ * });
+ * 
+ * // TypeError: Expected 'uint' but received 'number'
+ * const uint = $uint.assert(-1);
+ * 
+ * // Ok
+ * const uint = $uint.assert(1);
+ * ```
+ */
 class Checker<T = unknown>
 {
 	/**
@@ -122,49 +141,69 @@ class CheckerError extends TypeError {
 const unionSeparator = ' | ';
 
 
-
+/** Checker that always fails */
 const $never: Checker<never> = new Checker<never>({
 	typeString: 'never',
 	check(_value) { return false },
 });
 
+/** Checker for undefined */
 const $undefined: Checker<undefined> = new Checker<undefined>({
 	typeString: 'undefined',
 	check(value) { return typeof value === 'undefined' },
 });
 
+/** Checker for null */
 const $null: Checker<null> = new Checker<null>({
 	typeString: 'null',
 	check(value) { return value === null },
 });
 
+/** Checker for boolean */
 const $boolean: Checker<boolean> = new Checker<boolean>({
 	typeString: 'boolean',
 	check(value) { return typeof value === 'boolean' },
 });
 
+/** Checker for number */
 const $number: Checker<number> = new Checker<number>({
 	typeString: 'number',
 	check(value) { return typeof value === 'number' },
 });
 
+/** Checker for string */
 const $string: Checker<string> = new Checker<string>({
 	typeString: 'string',
 	check(value) { return typeof value === 'string' },
 });
 
+/** Checker for bigint */
 const $bigint: Checker<bigint> = new Checker<bigint>({
 	typeString: 'bigint',
 	check(value) { return typeof value === 'bigint' },
 });
 
+/** Checker for symbol */
 const $symbol: Checker<symbol> = new Checker<symbol>({
 	typeString: 'symbol',
 	check(value) { return typeof value === 'symbol' },
 });
 
 
-
+/** Checker factory for arrays
+ * @example
+ * ```ts
+ * const $numbers = $array($number);
+ * $numbers.assert([1, 2, '3']); // Error
+ * $numbers.assert([1, 2, 3]);   // Ok
+ * ```
+ * @example
+ * ```ts
+ * const $numbersLoose = $array($number, $string);
+ * $numbersLoose.assert([1, 2, '3']);       // Ok
+ * $numbersLoose.assert([1, 2, '3', true]); // Error
+ * ```
+ */
 function $array<T extends Checker[] | [Checker]>(...checkers: T): Checker<CheckerType<T[number]>[]> {
 
 	let checker: Checker;
@@ -188,6 +227,17 @@ function $array<T extends Checker[] | [Checker]>(...checkers: T): Checker<Checke
 	});
 }
 
+/** Checker factory for objects with explicitly declared keys
+ * @example
+ * ```ts
+ * const $person = $object({
+ * 	name: $string,
+ * 	age: $number
+ * });
+ * 
+ * const person = $person.assert(...);
+ * ```
+ */
 function $object<T extends Record<string, Checker>>(record: T): Checker<{ [K in keyof T]: CheckerType<T[K]> }> {
 
 	const checkerEntries = Object.entries(record);
@@ -214,8 +264,21 @@ function $object<T extends Record<string, Checker>>(record: T): Checker<{ [K in 
 	});
 }
 
-/** Similar to object but will also fail if extra properties
- * exist even if the type of the expected properties matche */
+/** Similar to $object, but also fails if the value has extra properties
+ * @example
+ * ```ts
+ * const $person = $objectStrict({
+ * 	name: $string,
+ * 	age: $number
+ * });
+ * 
+ * const person = $person.assert({
+ * 	name: 'John',
+ * 	age: 30,
+ * 	pets: 2
+ * });
+ * ```
+ */
 function $objectStrict<T extends Record<string, Checker>>(record: T): Checker<{ [K in keyof T]: CheckerType<T[K]> }> {
 
 	const checkerEntries = Object.entries(record);
@@ -246,6 +309,24 @@ function $objectStrict<T extends Record<string, Checker>>(record: T): Checker<{ 
 	});
 }
 
+/** Checker factory for objects with unknown keys
+ * @example
+ * ```ts
+ * const $checker = $record($number);
+ * 
+ * // Error
+ * $checker.assert({
+ * 	foo: 'a',
+ * 	bar: 'b'
+ * });
+ * 
+ * // Ok
+ * $checker.assert({
+ * 	foo: 1,
+ * 	bar: 2
+ * });
+ * ```
+ */
 function $record<T extends Checker>(valueChecker: T): Checker<Record<string, CheckerType<T>>> {
 	return new Checker({
 
@@ -262,7 +343,24 @@ function $record<T extends Checker>(valueChecker: T): Checker<Record<string, Che
 	});
 }
 
-
+/**
+ * Checker for literal values
+ * @example
+ * ```ts
+ * const $color = $literal('red', 'green', 'blue');
+ * $color.assert('yellow'); // Error
+ * $color.assert('red');    // Ok
+ * 
+ * const $code = $literal(100, 200, 300, 400, 500);
+ * $code.assert(600); // Error
+ * $code.assert(100); // Ok
+ * 
+ * const $version = $literal(1, '1', 2, '2', 3, '3');
+ * $version.assert(4);   // Error
+ * $version.assert('3'); // Ok
+ * $version.assert(1);   // Ok
+ * ```
+ */
 function $literal<T extends (string | number)[]>(...literals: T): Checker<T[number]> {
 
 	const typeString = literals
@@ -280,6 +378,7 @@ function $literal<T extends (string | number)[]>(...literals: T): Checker<T[numb
 	});
 }
 
+/** Checker factory for union types of the type passed and undefined */
 function $optional<T extends Checker>(checker: T): Checker<CheckerType<T> | undefined> {
 	return new Checker({
 
@@ -292,6 +391,7 @@ function $optional<T extends Checker>(checker: T): Checker<CheckerType<T> | unde
 	});
 }
 
+/** Checker factory for union types of the type passed and null */
 function $nullable<T extends Checker>(checker: T): Checker<CheckerType<T> | null> {
 	return new Checker({
 
@@ -304,6 +404,15 @@ function $nullable<T extends Checker>(checker: T): Checker<CheckerType<T> | null
 	});
 }
 
+/** Checker factory for tuples
+ * @example
+ * const $keyValuePair = $tuple($string, $number);
+ * $keyValuePair.assert([1000]);                // Error
+ * $keyValuePair.assert(['score']);             // Error
+ * $keyValuePair.assert([1000, 'score']);       // Error
+ * $keyValuePair.assert(['score', 1000, true]); // Error
+ * $keyValuePair.assert(['score', 1000]);       // Ok
+ */
 function $tuple<T extends Checker[]>(...checkers: T): Checker<ExtractTuple<T>> {
 	return new Checker({
 
@@ -320,6 +429,13 @@ function $tuple<T extends Checker[]>(...checkers: T): Checker<ExtractTuple<T>> {
 	});
 }
 
+/** Checker factory for tuples
+ * @example
+ * const $textOrNum = $union($string, $number);
+ * $textOrNum.assert(true);    // Error
+ * $textOrNum.assert('hello'); // Ok
+ * $textOrNum.assert(1000);    // Ok
+ */
 function $union<T extends Checker[]>(...checkers: T): Checker<CheckerType<T[number]>> {
 	return new Checker({
 
